9a8119aa84ca675805aa6797bc1a73b77f76baf6
Songbai Yan <colorguitar@hotmail.com>
2018-11-30 19:42:52 +0800

    refactor

--- src/main/java/butterfly/Downloader.java
+++ src/main/java/butterfly/Downloader.java
@@ -9,44 +9,38 @@ public class Downloader {
     private String savePath;
 
     public Downloader(String savePath) {
-
         this.savePath = savePath;
     }
 
     public void download(String url) throws IOException {
         String filename = url.substring(url.lastIndexOf('/') + 1);
-        saveImage(url, filename);
+        saveImage(url, savePath + filename);
     }
 
-    private InputStream read(URL url) {
-        try {
-            HttpURLConnection httpcon = (HttpURLConnection) url.openConnection();
-            httpcon.addRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36");
-            httpcon.addRequestProperty("Referer", "https://www.meitulu.com/item/15269.html");
+    private InputStream read(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36");
+        connection.addRequestProperty("Referer", "https://www.meitulu.com/item/15269.html");
 
-            int responseCode = httpcon.getResponseCode();
-            System.out.println(responseCode);
-            InputStream stream = httpcon.getErrorStream();
-            if (stream == null) {
-                stream = httpcon.getInputStream();
-                return stream;
-            } else {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-                String line = null;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                }
+        int responseCode = connection.getResponseCode();
+        System.out.println(responseCode);
+        InputStream stream = connection.getErrorStream();
+        if (stream == null) {
+            stream = connection.getInputStream();
+            return stream;
+        } else {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
             }
-            throw new RuntimeException("123");
-        } catch (IOException e) {
-            String error = e.toString();
-            throw new RuntimeException(e);
         }
+        throw new IOException("No image");
     }
 
     private void saveImage(String imageUrl, String destinationFile) throws IOException {
         URL url = new URL(imageUrl);
-        InputStream is = read(url);//url.openStream();
+        InputStream is = read(url);
         OutputStream os = new FileOutputStream(destinationFile);
 
         byte[] b = new byte[2048];
--- src/test/java/butterfly/DownloaderTest.java
+++ src/test/java/butterfly/DownloaderTest.java
@@ -7,9 +7,7 @@ import java.io.IOException;
 public class DownloaderTest {
     @Test
     public void testDownloadImage() throws IOException {
-        Downloader downloader = new Downloader("/Users/songbai.yan/Desktop/123/");
+        Downloader downloader = new Downloader("/Users/123/");
         downloader.download("https://mtl.ttsqgs.com/images/img/15269/1.jpg");
-        //https://mtl.ttsqgs.com/images/img/15269/1.jpg
-        //https://mtl.ttsqgs.com/images/img/15332/1.jpg
     }
 }
